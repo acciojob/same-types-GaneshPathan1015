6e8a13b589fd18994293e7c2dedb430964f124b1
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-08 14:12:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function isSameType(value1, value2) {
   //your js code here
 	let typeof1 = (typeof value1);
 	let typeof2 = (typeof value2);
-
-	let ans = (typeof1===typeof2)?true:false;
+	let ans
+	 return ans = (typeof1===typeof2)?true:false;
 	console.log(ans);
 	
 }
