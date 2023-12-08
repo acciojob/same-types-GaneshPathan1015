32493cba2404021af2f2faf312ceaa1df3db3183
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-08 16:03:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,6 +12,8 @@
 // let value1 = prompt("Enter Start of the Range.");
 // let value2 = prompt("Enter End Of the Range.");
 // alert(isSameType(value1, value2));
+
+
 function isSameType(value1, value2) {
   if (isNan(value1) || isNan(value2)) {
     return isNan(value1) === isNan(value2);
@@ -19,11 +21,15 @@ function isSameType(value1, value2) {
   return toString.call(value1) === toString.call(value2);
 }
 
-function isNan(val) {
-  return val !== val;
-}
+// function isNan(val) {
+//   return val !== val;
+// }
 
+// uncoment first
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
+
+
+
